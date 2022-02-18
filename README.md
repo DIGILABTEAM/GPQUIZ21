a6650558bbc7a9a24d7c94060b5a7c96257ac427
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2022-02-18 21:03:24 +0530

    deploy new app

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>Developed By Digilabs</p>
+# GP-QUIZ 
+Developed By Digilab <a href="https://digilabteam.github.io/GPQUIZ20/">GP GAME </a>
---
+++ js/questions.js
@@ -0,0 +1,44 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "when should be Systemic antitussives recommended",
+				option1: "For a productive cough that interferes with sleep",
+				option2: "Any type of cough",
+				option3: "Usually not recommended",
+				answer: 1
+			},
+			{
+				question: "The type of emphysema associated with smoking is",
+				option1: "Panacinar",
+				option2: "Centriacinar",
+				option3: "Distal acinar",
+				option4: "Irregular",
+				answer: 2
+			},
+			{
+				question: "Which of the following should not be used to treat a productive cough?",
+				option1: "Guaifenesin",
+				option2: "Dextromethorphan",
+				option3: "Diphenhydramine",
+				option4: "Simple linctus",	
+				answer: 2
+			},
+			{
+				question: "Which drug depolymerises mucopolysaccharides of mucus?",
+				option1: "Guaiphenesin",
+				option2: "Nascopine",
+				option3: "Bromohexine",
+				option4: "Diphenhydramine",
+				answer: 3
+			},
+			{
+				question: "when should be Systemic antitussives recommended",
+				option1: "For a productive cough that interferes with sleep",
+				option2: "Any type of cough",
+				option3: "Usually not recommended",
+				answer: 1
+			}
+		]
+	];
